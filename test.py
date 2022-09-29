60d02d4b8769e04aff6cf8960b70c89f99c75193
ryantien3 <ryantien3226@gmail.com>
2022-09-29 16:57:36 -0700

    Removing test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-if _name_ = '_main_':
-    x = "ECE_180_DA_DB"
-    if x == "EE_180_DA_DB":
-        print ("You are living in 2017")
-    else:
-        # this is a comment 
-        x = x + " - Best class ever"
-        print(x)
